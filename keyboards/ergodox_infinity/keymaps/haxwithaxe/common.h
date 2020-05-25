3386a0dd2ef8f39f9e7f6a1379741e0053d68ec2
haxwithaxe <me@haxwithaxe.net>
2020-05-25 14:01:41 -0400

    style/formatting fixes and broke out some values into a header file

---
+++ keyboards/ergodox_infinity/keymaps/haxwithaxe/common.h
@@ -0,0 +1,53 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/*
+ * Ln_COLOR is hue.
+ *
+ */
+
+#define L0 0 // default layer
+#define L0_NAME "0"
+#define L0_COLOR 0  // red
+
+#define L1 1
+#define L1_NAME "1:Control"
+#define L1_COLOR 20  // redish orange
+
+#define L2 2
+#define L2_NAME "2:FnNum"
+#define L2_COLOR 230  // electric blue
+
+#define L3 3
+#define L3_NAME "3:DD"
+#define L3_COLOR 40  // light orange
+
+#define L4 4
+#define L4_NAME "4:DDControl"
+#define L4_COLOR 50  // goldenrodish
+
+#define L5 5
+#define L5_NAME "5:Conf"
+#define L5_COLOR 300  // magenta
+
+#define L6 6
+#define L6_NAME "6:Noop"
+#define L6_COLOR 160  // teal
+
+#define L7 7
+#define L7_NAME "7:Noop"
+#define L7_COLOR 177  // cyan
+
+#define L8 8
+#define L8_NAME "8:Noop"
+#define L8_COLOR 188  // sky blue
+
+#define L9 9
+#define L9_NAME "9:Noop"
+#define L9_COLOR 199  // powder blue
+
+#define L10 10
+#define L10_NAME "10:Noop"
+#define L10_COLOR 322  // pink
+
+#endif
